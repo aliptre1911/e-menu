d288e7162c24f2218f5dd750471b5e38ca52423f
ali <s11113151@stu.edu.tw>
2013-01-16 12:02:47 +0800

    page2 - under_bar and top_bar

--- src/com/example/e_menu/preselectedActivity.java
+++ src/com/example/e_menu/preselectedActivity.java
@@ -1,8 +1,14 @@
 package com.example.e_menu;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.LinearLayout;
 
 public class preselectedActivity extends Activity {
 
@@ -11,5 +17,9 @@ public class preselectedActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.preselected_meal);
+	}
+
+	public void back(View v) {
+		finish();
 	}
 }
