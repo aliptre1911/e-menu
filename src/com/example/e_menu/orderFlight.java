08c9312f414d2133d15ab0d0757a2ed3e3a22a73
ali <s11113151@stu.edu.tw>
2013-01-17 21:34:47 +0800

    page3 list title

--- src/com/example/e_menu/orderFlight.java
+++ src/com/example/e_menu/orderFlight.java
@@ -1,25 +1,19 @@
 package com.example.e_menu;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
-public class preselectedActivity extends Activity {
-
+public class orderFlight extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.preselected_meal);
+		setContentView(R.layout.order_flight);
 	}
 
 	public void back(View v) {
-		preselectedActivity.this.finish();
-	}
-
-	public void confirm(View v) {
-		startActivity(new Intent(this, orderFlight.class));
+		orderFlight.this.finish();
 	}
 }
