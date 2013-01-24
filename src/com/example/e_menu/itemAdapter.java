8857f1c96bd54867f63ff1fb651e4106ac770dfc
ali <s11113151@stu.edu.tw>
2013-01-24 16:54:13 +0800

    radiobutton in listview

---
+++ src/com/example/e_menu/itemAdapter.java
@@ -0,0 +1,93 @@
+package com.example.e_menu;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class itemAdapter extends BaseAdapter {
+
+	private LayoutInflater myInflater;
+	private ArrayList<String> list = null;
+	private int selectedIndex;
+	private String indexArr[] = new String[26];
+
+	public itemAdapter(Context ctxt, ArrayList<String> list, int selectedIndex) {
+		myInflater = LayoutInflater.from(ctxt);
+		this.list = list;
+		this.selectedIndex = selectedIndex;
+		// A ~ Z
+		for (int i = 65; i <= 90; i++)
+			indexArr[i - 65] = (char) i + ".";
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return list.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return arg0;
+	}
+
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		MyView holder;
+
+		if (convertView == null) {
+			convertView = myInflater.inflate(R.layout.item, null);
+			holder = new MyView(
+					(RadioButton) convertView.findViewById(R.id.item_btn),
+					(TextView) convertView.findViewById(R.id.item_no),
+					(TextView) convertView.findViewById(R.id.item));
+			convertView.setTag(holder);
+		} else {
+			holder = (MyView) convertView.getTag();
+		}
+
+		// set text
+		holder.item_no.setText(indexArr[position]);
+		holder.item.setText(list.get(position));
+
+		// ¿ï¨ú
+		holder.itemBtn.setChecked(position == selectedIndex ? true : false);
+		convertView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				selectedIndex = position;
+				itemAdapter.this.notifyDataSetChanged();
+			}
+		});
+		return convertView;
+	}
+
+	class MyView {
+		RadioButton itemBtn;
+		TextView item_no;
+		TextView item;
+
+		public MyView(RadioButton itemBtn, TextView item_no, TextView item) {
+			this.itemBtn = itemBtn;
+			this.item_no = item_no;
+			this.item = item;
+		}
+	}
+}
--- src/com/example/e_menu/orderDinner.java
+++ src/com/example/e_menu/orderDinner.java
@@ -1,5 +1,8 @@
 package com.example.e_menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -19,6 +22,11 @@ public class orderDinner extends Activity {
 	String name = "Mary Lin";
 	String storke = "AKL-BNE-TPE";
 	Bundle bData;
+	ArrayList<String> mainList;
+	ArrayList<String> pairList;
+	String mainDinnerArray[] = { "Á¤¨ýÂæ¥Ä¯Ã¦èÄõ¬õ³½¦õªãÄõ¡B­»Û£¡B¬õÅÚ½³¤Î¦Ì¶º",
+			"­»·Î¤ûµá¤O¦õºî¦X½­µæ¡B¥É¦Ì¬v¨¡»T¤Îºñ­J´ÔÂæ", "¥\¤ÒÂû-ªk¦¡§C·ÅªoµN¦õ±m´Ô¡B¸`¥Ê¡BÄªµ«¡B¬õÅÚ½³¡B¸q¦¡¼[¶º»æ¤Î¥¤ªoÄ¨Û£Âæ" };
+	String pairDinnerArray[] = { "»¶¨ý­»¾í½¼¤¯¤z¨©¦õ«Cªáµæ¡B¬õÅÚ½³¡B­»Û£¤Î¶Àª÷ª£¶º" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +56,18 @@ public class orderDinner extends Activity {
 		tv_seat.setText(bData.getStringArray("data")[2] + " "
 				+ bData.getStringArray("data")[4]);
 		tv_storke.setText(this.storke);
-		// { "2012-09-15", "10:35", "CI0054", "AKLTPE", "°Ó°È¿µ"
+		// list
+		mainList = new ArrayList<String>();
+		pairList = new ArrayList<String>();
+		arrayPushDatatoList(mainList, mainDinnerArray);
+		arrayPushDatatoList(pairList, pairDinnerArray);
+		mainDinnerList.setAdapter(new itemAdapter(this, mainList, 0));
+		pairDinnerList.setAdapter(new itemAdapter(this, pairList, 0));
+	}
+
+	private void arrayPushDatatoList(ArrayList<String> list, String arr[]) {
+		for (String data : arr)
+			list.add(data);
 	}
 
 	private void findview() {
@@ -57,5 +76,7 @@ public class orderDinner extends Activity {
 		tv_seat = (TextView) findViewById(R.id.tv_seat);
 		tv_departure_date = (TextView) findViewById(R.id.tv_departure_date);
 		tv_storke = (TextView) findViewById(R.id.tv_stroke);
+		mainDinnerList = (ListView) findViewById(R.id.mainDinnerList);
+		pairDinnerList = (ListView) findViewById(R.id.pairDinnerList);
 	}
 }
